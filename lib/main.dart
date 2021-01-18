5a501b8e26429353729764be96e5b29b1cfa944e
noman-khalid-dev <noman.khalid.freelance@gmail.com>
2021-01-18 17:50:50 +0500

    Insert my pic

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,26 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
-  );
+    MyApp()
+  );//end runApp
+}//end main
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.teal,
+          body: SafeArea(
+            child: Column(
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage("images/noman.jpeg"),
+                ),
+              ],
+            ),
+          )
+        ),
+      );
+  }
 }
